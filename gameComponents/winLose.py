2323a7b876d1e39deee72cdedadf7f465cd9eb8e
artem Tsukanov <77419153+ArtemTsukanovv@users.noreply.github.com>
2021-03-27 04:45:13 +0200

    Update winLose.py

--- gameComponents/winLose.py
+++ gameComponents/winLose.py
@@ -1,13 +1,13 @@
 from gameComponents import gameVars
 
-# define a win / lose function and refer to it (invoke it) in our game loop
+
 def winorlose(status):
     if status == "won":
-        pre_message = "You are the huuuuuuugest winner ever! "
+        pre_message = "You win! "
     else:
-        pre_message = "You done trumped it, loser! "
+        pre_message = "You lose! "
 
-    print(pre_message + 'Would you like to play again?')
+    print(pre_message + 'Play again?)')
 
     choice = False
 
@@ -15,13 +15,11 @@ def winorlose(status):
         choice = input("Y / N? ")
 
         if choice == "Y" or choice == "y":
-            # reset the game loop and start over again
             gameVars.player_lives = gameVars.total_lives
             gameVars.computer_lives = gameVars.total_lives
             gameVars.player_choice = False
         elif choice == "N" or choice == "n":
-            # exit message and quit
-            print("You chose to quit. Better luck next time!")
+            print("You chose to quit.")
             exit()
         else:
             print("Make a valid choice - Y or N")
